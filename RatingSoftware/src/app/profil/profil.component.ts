a8dc8e38ad4afb3d97bb52ea998e141daca3e888
christopherad <christopher.di.pro@gmail.com>
2022-03-22 12:58:07 +0100

    profil-v1

---
+++ RatingSoftware/src/app/profil/profil.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profil',
+  templateUrl: './profil.component.html',
+  styleUrls: ['./profil.component.scss']
+})
+export class ProfilComponent implements OnInit {
+
+  photoProfil:any = "../assets/Unsplash-Avatars_0003s_0012_alex-lacayo-hchKfNuAblU-unsplash.png"
+  pseudo:String = "pseudo"
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
