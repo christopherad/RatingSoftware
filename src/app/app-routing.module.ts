ca24714e708d4709256275020fb39241ed9919a3
Geniez91 <weltmannjeremy@gmail.com>
2022-03-23 17:51:26 +0100

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404Component } from './components/error404/error404.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MoviesComponent } from './pages/movies/movies.component';
 
@@ -7,6 +8,7 @@ const routes: Routes = [
     {path:"", redirectTo:"/Home",pathMatch:"full"},
       {path:"Home",component:HomeComponent},
       {path:"Movie/:id",component:MoviesComponent},
+      {path:"**",component:Error404Component}
 ];
 
 @NgModule({
