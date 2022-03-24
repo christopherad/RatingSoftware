489f2fa93391f27c7e6644f38b8f041b5286235d
christopherad <christopher.di.pro@gmail.com>
2022-03-24 17:02:37 +0100

    profil + footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  logo:any = "../assets/Group24.png"
+  iconeFB:any = "../assets/icone-facebook-ronde.png"
+  iconeTW:any = "../assets/icone-twitter-ronde.png"
+  iconeIG:any = "../assets/240px-Instagram-Icon.png"
+  iconeGH:any = "../assets/25231.png"
   constructor() { }
 
   ngOnInit(): void {
