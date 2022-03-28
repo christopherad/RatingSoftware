c1ec54fa001debdee8b8c92faf225816c1d1d526
christopherad <christopher.di.pro@gmail.com>
2022-03-28 17:32:05 +0200

    Login + profil + footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { MoviesComponent } from './pages/movies/movies.component';
 import { SearchComponent } from './pages/search/search.component';
 import { TvshowComponent } from './pages/tvshow/tvshow.component';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/Home', pathMatch: 'full' },
   { path: 'Home', component: HomeComponent },
+  { path: 'Login', component: LoginComponent },
   { path: 'Search/:id', component: SearchComponent },
   { path: 'Search', component: SearchComponent },
   { path: 'Movie/:id', component: MoviesComponent },
