23a5b8e3f605efc75d9d03d71634bb9466ca1e5f
ferdok <ferdok@localhost.localdomain>
2022-04-05 02:06:24 +0200

    email config

--- config/plugins.js
+++ config/plugins.js
@@ -1,9 +1,19 @@
 // config/plugins.js
 
-module.exports = {
-    'raw-query': {
-      enabled: true,
+module.exports = ({ env }) => ({
+  'raw-query': {
+    enabled: true,
+  },
+  email: {
+    config: {
+      provider: 'sendgrid',
+      providerOptions: {
+        apiKey: env('SENDGRID_API_KEY'),
+      },
+      settings: {
+        defaultFrom: 'frdndattivi@gmail.com',
+        defaultReplyTo: 'frdndattivi@gmail.com',
+      },
     },
-    // ...
-  }
-  
+  },
+  });
