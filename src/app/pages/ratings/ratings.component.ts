678931bccd0167c2fc46b2111b3eed2d53a68e20
fahimgha <fahimgha10@email.com>
2022-04-16 19:08:09 +0200

    update ratings page

--- src/app/pages/ratings/ratings.component.ts
+++ src/app/pages/ratings/ratings.component.ts
@@ -11,94 +11,100 @@ import { User } from 'src/app/services/Register/register.service';
 @Component({
   selector: 'app-ratings',
   templateUrl: './ratings.component.html',
-  styleUrls: ['./ratings.component.css']
+  styleUrls: ['./ratings.component.css'],
 })
 export class RatingsComponent implements OnInit {
-   EmojiContent:any="../../assets/Images/EmojiContent.png";
-   EmojiEnnuye:any="../../assets/Images/EmojiEnnuyé.png";
-   EmojiTriste:any="../../assets/Images/EmojiTriste.png";
-   EmojiAmuse:any="../../assets/Images/EmojiAmusé.png";
-   EmojiChoque:any="../../assets/Images/EmojiChoqué.png";
-   Listcritiere:Object[]=[]
-    ObjectCritiere1!:Object;
-     ObjectCritiere2!:Object;
-      ObjectCritiere3!:Object;
-     isMessage:boolean=false
+  EmojiContent: any = '../../assets/Images/EmojiContent.png';
+  EmojiEnnuye: any = '../../assets/Images/emoji-ennuye.png';
+  EmojiTriste: any = '../../assets/Images/emoji-triste.png';
+  EmojiAmuse: any = '../../assets/Images/emoji-amuse.png';
+  EmojiChoque: any = '../../assets/Images/emoji-choque.png';
+  content: any = '../../assets/Images/content.png';
+  Listcritiere: Object[] = [];
+  ObjectCritiere1!: Object;
+  ObjectCritiere2!: Object;
+  ObjectCritiere3!: Object;
+  isMessage: boolean = false;
 
-   note1!:number
-   RatingsForm=new FormGroup({
-     recommendation:new FormControl(''),
-     critique:new FormControl(''),
-     critere1:new FormControl(''),
-      critere2:new FormControl(''),
-       critere3:new FormControl(''),
-     ressenti:new FormControl(''),
-   })
-rate?:number
-rate2?:number
-rate3?:number
-rate4?:number
+  note1!: number;
+  RatingsForm = new FormGroup({
+    recommendation: new FormControl(''),
+    critique: new FormControl(''),
+    critere1: new FormControl(''),
+    critere2: new FormControl(''),
+    critere3: new FormControl(''),
+    ressenti: new FormControl(''),
+  });
+  rate?: number;
+  rate2?: number;
+  rate3?: number;
+  rate4?: number;
   cookieValue!: string;
   user!: User;
   idFilm: any;
-  constructor(private cookieService: CookieService,private api:MoviesServices,private route: ActivatedRoute) {
-this.rate=1
-this.rate2=1
-this.rate3=1;
-this.rate4=1;
-this.cookieValue = this.cookieService.get('user');
-  this.idFilm=this.route.snapshot.paramMap.get('id')!
+  constructor(
+    private cookieService: CookieService,
+    private api: MoviesServices,
+    private route: ActivatedRoute
+  ) {
+    this.rate = 1;
+    this.rate2 = 1;
+    this.rate3 = 1;
+    this.rate4 = 1;
+    this.cookieValue = this.cookieService.get('user');
+    this.idFilm = this.route.snapshot.paramMap.get('id')!;
   }
 
   ngOnInit(): void {
-
-  axios.get('http://51.158.72.178:1337/api/users/me ', {
-    headers: {
-      Authorization: `Bearer ${this.cookieValue}`,
-    },
-  })
-  .then((response) => {
-    // Handle success.
-    this.user=response.data
-    console.log('Data: ', this.user);
-  })
-  .catch((error) => {
-    // Handle error.
-    console.log('An error occurred:', error.response);
-  });
-
+    axios
+      .get('http://51.158.72.178:1337/api/users/me ', {
+        headers: {
+          Authorization: `Bearer ${this.cookieValue}`,
+        },
+      })
+      .then((response) => {
+        // Handle success.
+        this.user = response.data;
+        console.log('Data: ', this.user);
+      })
+      .catch((error) => {
+        // Handle error.
+        console.log('An error occurred:', error.response);
+      });
   }
-  onSubmit(){
-    this.Listcritiere=[]
-    this.ObjectCritiere1={
-      name:"Cinematographie",
-      note:this.RatingsForm.value.critere1
-    }
-    this.ObjectCritiere2={
-      name:"Histoire",
-      note:this.RatingsForm.value.critere2
-    }
-     this.ObjectCritiere3={
-      name:"Autre",
-      note:this.RatingsForm.value.critere3
-    }
-    this.Listcritiere.push(this.ObjectCritiere1)
-    this.Listcritiere.push(this.ObjectCritiere2)
-    this.Listcritiere.push(this.ObjectCritiere3)
-    console.log(this.RatingsForm.value)
-    console.log(this.Listcritiere)
-    const {critique,recommendation,ressenti}=this.RatingsForm.value
-    
-   this.api.AddRatings(this.user.id,this.idFilm,this.Listcritiere,recommendation,critique,ressenti,this.cookieValue)
-   .then(response=>{
-     this.isMessage=true;
-   })
-    
+  onSubmit() {
+    this.Listcritiere = [];
+    this.ObjectCritiere1 = {
+      name: 'Cinematographie',
+      note: this.RatingsForm.value.critere1,
+    };
+    this.ObjectCritiere2 = {
+      name: 'Histoire',
+      note: this.RatingsForm.value.critere2,
+    };
+    this.ObjectCritiere3 = {
+      name: 'Autre',
+      note: this.RatingsForm.value.critere3,
+    };
+    this.Listcritiere.push(this.ObjectCritiere1);
+    this.Listcritiere.push(this.ObjectCritiere2);
+    this.Listcritiere.push(this.ObjectCritiere3);
+    console.log(this.RatingsForm.value);
+    console.log(this.Listcritiere);
+    const { critique, recommendation, ressenti } = this.RatingsForm.value;
 
-
-
+    this.api
+      .AddRatings(
+        this.user.id,
+        this.idFilm,
+        this.Listcritiere,
+        recommendation,
+        critique,
+        ressenti,
+        this.cookieValue
+      )
+      .then((response) => {
+        this.isMessage = true;
+      });
   }
- 
-
-
 }
