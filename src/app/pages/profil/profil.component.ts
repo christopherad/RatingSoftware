1bf98df6cbb81cc6782cb84269a125a8bf81b9ee
Geniez91 <weltmannjeremy@gmail.com>
2022-04-16 13:00:38 +0200

    profil

--- src/app/pages/profil/profil.component.ts
+++ src/app/pages/profil/profil.component.ts
@@ -5,6 +5,7 @@ import axios from 'axios';
 import { User } from 'src/app/services/Register/register.service';
 import { MoviesServices } from '../../services/Movies/Movies.service';
 import { Item } from 'src/app/models/Item';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-profil',
@@ -19,12 +20,61 @@ export class ProfilComponent implements OnInit {
   user!:User
   pseudo:String = "pseudo"
   ListFavoris!:Item[]
+  selectedFile!: File;
+  disabled=true;
   
   
-  constructor(private cookieService: CookieService,private api:MoviesServices) { 
+  constructor(private cookieService: CookieService,private api:MoviesServices,private http:HttpClient) { 
 this.cookieValue = this.cookieService.get('user');
         console.log(this.cookieValue)
   }
+    onFileChanged(event:any) {
+    this.selectedFile = event.target.files[0]
+    console.log(this.selectedFile)
+    this.disabled=false
+  }
+
+    onUpload() {
+      console.log(this.selectedFile)
+      if(this.selectedFile!==null){
+      /*  let test=new FormData()
+        test.append('image',this.se)
+         this.http.post('http://51.158.72.178:1337/api/upload/',{
+           files:this.selectedFile
+         }).subscribe(data=>{
+           console.log(data)
+         })
+*/
+let FormData1=new FormData()
+FormData1.append('image',this.selectedFile)
+axios
+  .post('http://51.158.72.178:1337/api/upload', {
+     files:FormData1
+  
+  },{
+    headers: {
+      'Authorization': `Bearer ${this.cookieValue}`,
+       'Content-Type': this.selectedFile.type,
+    'Accept': this.selectedFile.type,
+   
+    },
+  })
+  
+  .then(response => {
+    console.log(response)
+  })
+  .catch(error => {
+    console.log('An error occurred:', error.response);
+  });
+  
+      }
+      
+
+
+ 
+
+    }
+
 
   ngOnInit(): void {
         
